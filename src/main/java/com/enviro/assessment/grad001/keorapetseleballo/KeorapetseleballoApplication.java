c13d91fcf283d37c0309187bb6c17e46ca3efb6d
Keorapetse <kleballo023@student.wethinkcode.co.za>
2025-01-22 07:09:28 +0200

    first commit

---
+++ src/main/java/com/enviro/assessment/grad001/keorapetseleballo/KeorapetseleballoApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.keorapetseleballo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeorapetseleballoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeorapetseleballoApplication.class, args);
+	}
+
+}
